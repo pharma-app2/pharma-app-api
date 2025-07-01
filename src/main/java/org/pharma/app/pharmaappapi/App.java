bdf9028823c68213d6daf782754c247858c26b61
DaniloLO53 <daniloleaodev@gmail.com>
2025-07-01 09:19:15 -0300

    [init][config] initial configurations

---
+++ src/main/java/org/pharma/app/pharmaappapi/App.java
@@ -0,0 +1,13 @@
+package org.pharma.app.pharmaappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ src/test/java/org/pharma/app/pharmaappapi/PharmaAppApiApplicationTests.java
@@ -0,0 +1,15 @@
+package org.pharma.app.pharmaappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PharmaAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
