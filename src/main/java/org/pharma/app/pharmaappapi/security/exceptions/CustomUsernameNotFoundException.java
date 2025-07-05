44a9047b4647a56f7a0c936aa490282e7aa21c38
DaniloLO53 <daniloleaodev@gmail.com>
2025-07-05 10:46:43 -0300

    [refactor] deleted customUsernameNotFoundException (now using default spring-security exception)

--- src/main/java/org/pharma/app/pharmaappapi/security/exceptions/CustomUsernameNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.pharma.app.pharmaappapi.security.exceptions;
-
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public class CustomUsernameNotFoundException extends UsernameNotFoundException {
-    public CustomUsernameNotFoundException(String fieldName, String fieldValue) {
-        super(String.format("User with %s %s not found.", fieldName, fieldValue));
-    }
-}
